01e5b902be4a350a0021dc520bcf4ce775465bf9
Keith McKay <keith.mckay@bombbomb.com>
2021-12-16 10:05:15 -0700

    Fixed build script and added js file to it

---
+++ public/scripts-for-ketih-and-bre.js
@@ -0,0 +1,28 @@
+// This is the pop up that ask for our dogs names
+
+// let answer;
+// let greeting;
+
+// function askForDoggos () {
+//     const answer = prompt("What is the name of one of our dogs?");
+//     setTimeout( () => {
+//         if (answer === "Toph" || answer === "Iroh" ) {
+//             greeting = alert("Welcome to our website!");
+//             console.log(`The answer was ${answer}`)
+//          } else {
+//              greeting = alert('You probably shouldnt be here');  
+//              console.log(`The answer was ${answer}`)
+//          }
+
+//     } )
+// }
+
+
+// Image Gallery Script
+
+
+
+
+
+
+
